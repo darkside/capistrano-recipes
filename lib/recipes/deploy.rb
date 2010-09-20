7674bd5395e07995fa857037576c0306fada2b43
Darkside <leonardobighetti@gmail.com>
2010-09-20 09:52:19 -0300

    Added github-style deploy

--- lib/recipes/deploy.rb
+++ lib/recipes/deploy.rb
@@ -7,6 +7,54 @@ Capistrano::Configuration.instance(:must_exist).load do
   
   
   namespace :deploy do
+    desc "Deploy it, github-style."
+    task :default do
+      update
+      restart
+    end
+    
+    desc "[Seppuku] Destroy everything"
+    task :seppuku do
+      run "rm -rf #{current_path}; rm -rf #{shared_path}"
+      rubycas.seppuku
+    end
+    
+     task :setup_dirs, :except => { :no_release => true } do
+      commands = shared_dirs.map do |path|
+        "mkdir -p #{shared_path}/#{path}"
+      end
+      run commands.join(" && ")
+    end
+    
+    desc "Uploads your local config.yml to the server"
+    task :configure, :except => { :no_release => true } do
+      generate_config('config/config.yml', "#{shared_path}/config/config.yml")
+    end
+    
+    desc "Setup a GitHub-style deployment."
+    task :setup, :except => { :no_release => true } do
+      run "rm -rf #{current_path}"
+      setup_dirs
+      run "git clone #{repository} #{current_path}"
+    end
+    
+    desc "Update the deployed code."
+    task :update_code, :except => { :no_release => true } do
+      run "cd #{current_path}; git fetch origin; git reset --hard #{branch}"
+    end
+
+    task :symlink, :except => { :no_release => true } do
+      symlinks.make
+    end
+
+    namespace :rollback do
+      desc "Rollback a single commit."
+      task :default, :except => { :no_release => true } do
+        set :branch, "HEAD^"
+        deploy.default
+      end    
+    end
+
     desc <<-DESC
       Restarts your application. This depends heavily on what server you're running. 
       If you are running Phusion Passenger, you can explicitly set the server type:
