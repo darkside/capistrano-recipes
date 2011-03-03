d9fc8b145ccd9f5f817813dcb84da3c8e531ce6c
Darkside <leonardobighetti@gmail.com>
2011-03-03 15:12:15 -0300

    Version bump to 0.7.2

--- lib/recipes/sphinx.rb
+++ lib/recipes/sphinx.rb
@@ -4,27 +4,27 @@ Capistrano::Configuration.instance.load do
     task :config, :roles => :app do
       run "cd #{current_path}; #{rake_bin} RAILS_ENV=#{rails_env} ts:config"
     end
- 
+
     desc "|DarkRecipes| Starts TS"
     task :start, :roles => :app do
       run "cd #{current_path}; #{rake_bin} RAILS_ENV=#{rails_env} ts:start"
     end
- 
+
     desc "|DarkRecipes| Stops TS"
     task :stop, :roles => :app do
       run "cd #{current_path}; #{rake_bin} RAILS_ENV=#{rails_env} ts:stop"
     end
- 
+
     desc "|DarkRecipes| Rebuild TS"
     task :rebuild, :roles => :app do
       run "cd #{current_path}; #{rake_bin} RAILS_ENV=#{rails_env} ts:rebuild"
     end
- 
+
     desc "|DarkRecipes| Indexes TS"
     task :index, :roles => :app do
       run "cd #{current_path}; #{rake_bin} RAILS_ENV=#{rails_env} ts:in"
     end
-   
+
     desc "|DarkRecipes| Re-establishes symlinks"
     task :symlinks do
       run <<-CMD
@@ -32,8 +32,7 @@ Capistrano::Configuration.instance.load do
       CMD
     end
   end
-  
-  after "deploy:migrate" do 
-    sphinx.rebuild
-  end unless is_app_monitored?
+
+
 end
+
