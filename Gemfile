9f591930a9de06c0b39813aff5d614a9121ae90b
Leonardo Bighetti <leonardobighetti@gmail.com>
2011-07-12 13:12:40 -0300

    Bump to 0.8.1. Changes gemspec, some fixes, .rvmrc in project, updated jeweler support
    Signed-off-by: Leonardo Bighetti <leonardobighetti@gmail.com>

--- Gemfile
+++ Gemfile
@@ -1,4 +1,18 @@
 source "http://rubygems.org"
+# Add dependencies required to use your gem here.
+# Example:
+#   gem "activesupport", ">= 2.3.5"
 
-# Specify your gem's dependencies in capistrano-recipes.gemspec
-gemspec
+gem "capistrano",     ">= 2.5.9"
+gem "capistrano-ext", ">= 1.2.1"
+
+# Add dependencies to develop your gem here.
+# Include everything needed to run rake, tests, features, etc.
+group :development do
+  gem "shoulda", ">= 0"
+  gem "bundler", "~> 1.0.0"
+  gem "jeweler", "~> 1.6.4"
+  gem "rdoc", ">=2.4"
+  gem "rcov", ">= 0"
+end
+
--- lib/recipes/nginx.rb
+++ lib/recipes/nginx.rb
@@ -1,10 +1,15 @@
 Capistrano::Configuration.instance.load do
-    
+
   # Where your nginx lives. Usually /opt/nginx or /usr/local/nginx for source compiled.
   set :nginx_path_prefix, "/opt/nginx" unless exists?(:nginx_path_prefix)
 
   # Path to the nginx erb template to be parsed before uploading to remote
   set(:nginx_local_config) { "#{templates_path}/nginx.conf.erb" } unless exists?(:nginx_local_config)
+
+  # Init scripts
+  set(:nginx_init_local) { "#{docs_path}/nginx/nginx.init" }
+  set :nginx_init_temp,   '/tmp/nginx.init'
+  set :nginx_init_remote, '/etc/init.d/nginx'
 
   # Path to where your remote config will reside (I use a directory sites inside conf)
   set(:nginx_remote_config) do
@@ -18,22 +23,32 @@ Capistrano::Configuration.instance.load do
     task :setup, :roles => :app , :except => { :no_release => true } do
       generate_config(nginx_local_config, nginx_remote_config)
     end
-    
+
+    desc "|capistrano-recipes| Bootstraps Nginx to init.d"
+    task :setup_init, :roles => :app do
+      upload nginx_init_local, nginx_init_temp, :via => :scp
+      sudo "mv #{nginx_init_temp} #{nginx_init_remote}"
+      # Allow executing the init.d script
+      sudo "chmod +x #{nginx_init_remote}"
+      # Make it run at bootup
+      sudo "update-rc.d nginx defaults"
+    end
+
     desc "|capistrano-recipes| Parses config file and outputs it to STDOUT (internal task)"
     task :parse, :roles => :app , :except => { :no_release => true } do
       puts parse_config(nginx_local_config)
     end
-    
+
     desc "|capistrano-recipes| Restart nginx"
     task :restart, :roles => :app , :except => { :no_release => true } do
       sudo "service nginx restart"
     end
-    
+
     desc "|capistrano-recipes| Stop nginx"
     task :stop, :roles => :app , :except => { :no_release => true } do
       sudo "service nginx stop"
     end
-    
+
     desc "|capistrano-recipes| Start nginx"
     task :start, :roles => :app , :except => { :no_release => true } do
       sudo "service nginx start"
@@ -44,9 +59,9 @@ Capistrano::Configuration.instance.load do
       sudo "service nginx status"
     end
   end
-  
+
   after 'deploy:setup' do
     nginx.setup if Capistrano::CLI.ui.agree("Create nginx configuration file? [Yn]")
-  end if is_using_nginx 
+  end if is_using_nginx
 end
 
